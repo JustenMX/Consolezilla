0b7d4d4b2f3b5f34f84914b79b2bfb6109eca0a2
Justen Manni <mannijusten@gmail.com>
2022-08-27 15:36:41 +0800

    Project initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./main.css";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Footer from "./Footer";
+
+function LandingPage() {
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src="src/assets/logo gif/GIF Consolezilla Logo v1.gif"
+            loading="lazy"
+            className="max-w-sm rounded-lg shadow-2xl hover:shadow-lg"
+            alt="logo"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">WORK IN PROGRESS</h1>
+            <p className="py-6">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+              sem. Nulla consequat massa quis enim
+            </p>
+            {/* <button className="btn btn-primary">Get Started</button> */}
+          </div>
+        </div>
+      </div>
+      {/* Footer.jsx inserted here */}
+      <Footer />
+    </div>
+  );
+}
+
+export default LandingPage;
