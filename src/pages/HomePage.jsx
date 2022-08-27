5cbde1d1c01219b7990236101eb7861d381294df
Justen Manni <mannijusten@gmail.com>
2022-08-27 19:22:18 +0800

    Add HomePage and configurations

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import { useState, useEffect } from "react";
 
 function HomePage() {
   return (
     <>
       <NavBar />
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <h1>Homepage</h1>
+        </div>
+      </div>
       <Footer />
     </>
   );
