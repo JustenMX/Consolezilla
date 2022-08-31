63151775c088bfd167f7685c013a32bc486e2cd3
Justen Manni <mannijusten@gmail.com>
2022-08-31 22:17:58 +0800

    Update filter components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -101,49 +101,49 @@ function HomePage() {
             <button
               onClick={() => handleFilterGenre("action")}
               name="action"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               ACTION
             </button>
             <button
               onClick={() => handleFilterGenre("indie")}
               name="indie"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               INDIE
             </button>
             <button
               onClick={() => handleFilterGenre("racing")}
               name="rpg"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               RACING
             </button>
             <button
               onClick={() => handleFilterGenre("strategy")}
               name="action"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               STRATEGY
             </button>
             <button
               onClick={() => handleFilterGenre("shooter")}
               name="action"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               SHOOTER
             </button>
             <button
               onClick={() => handleFilterGenre("casual")}
               name="action"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               CASUAL
             </button>
             <button
               onClick={() => handleFilterGenre("simulation")}
               name="action"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               SIMULATION
             </button>
@@ -153,30 +153,37 @@ function HomePage() {
             <button
               onClick={() => handleFilterPlatforms("18")}
               name="action"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               PLAYSTATION 4
             </button>
             <button
               onClick={() => handleFilterPlatforms("187")}
               name="action"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               PLAYSTATION 5
             </button>
             <button
               onClick={() => handleFilterPlatforms("186")}
               name="action"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               XBOX SERIES S/X
             </button>
             <button
               onClick={() => handleFilterPlatforms("4")}
               name="action"
-              className="inline-block bg-indigo-500 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+              className="inline-block bg-emerald-800 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
             >
               GAMING PC
+            </button>
+            <button
+              onClick={() => handleFilterPlatforms("/")}
+              name="action"
+              className="inline-block bg-rose-700 hover:bg-indigo-700 active:bg-indigo-900 focus-visible:ring ring-indigo-300 border text-slate-100 text-sm md:text-base font-normal text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3 shadow-xl"
+            >
+              SHOW MORE +
             </button>
           </div>
           {/* filter buttons ends here */}
