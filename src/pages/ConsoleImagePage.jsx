38b238f1fb8e8f4b7e21325afc9b7ed2ab342e80
Justen Manni <mannijusten@gmail.com>
2022-09-02 01:20:31 +0800

    Add new pagination compoment for console components

--- src/pages/ConsoleImagePage.jsx
+++ src/pages/ConsoleImagePage.jsx
@@ -4,6 +4,7 @@ import { useParams, Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import Img from "react-cool-img";
+import PaginationConsole from "../components/PaginationConsole";
 
 function ConsoleImagePage() {
   const { id } = useParams();
@@ -20,9 +21,11 @@ function ConsoleImagePage() {
     <>
       <NavBar />
       <div className="bg-white dark:bg-slate-900 py-6 sm:py-8 lg:py-12">
-        <h2 className="text-emerald-800 dark:text-indigo-600 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">
-          Genres:
-        </h2>
+        <h2 className="text-emerald-800 dark:text-indigo-600 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6"></h2>
+        <PaginationConsole
+          consoleDetail={consoleDetail}
+          setConsoleDetail={setConsoleDetail}
+        />
         <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
           <div className="flex justify-between items-end gap-4 mb-6">
             <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-x-6 gap-y-8">
